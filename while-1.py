090eea49af1c1ffeb35b286fc2cccdeb44aa96db
Natalia <n.platova@hotmail.com>
2019-05-29 14:20:25 +0700

    exceptions-1

--- while-1.py
+++ while-1.py
@@ -2,12 +2,14 @@ def ask_user():
     questions = {"Как дела?": "Хорошо!", "Что делаешь?": "Программирую", "Где ты?": "Далеко", "Что мне делать?": "Поешь"}
 
     user_say = ""
-    while (user_say).strip() != "Хорошо":
-        user_say = input("Как дела?")
+    try:
+        while (user_say).strip() != "Хорошо":
+            user_say = input("Как дела?")
 
-    user_question = ""
-    while (user_question).strip() != "Stop":
         user_question = input("Задайте вопрос")
         print(questions.get((user_question).strip()))
+    except(KeyboardInterrupt):
+        print("Пока!")
+
 
 ask_user()
